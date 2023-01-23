38a3be56d10bb68bf497e9f7d0449d9b2464dece
최서연 <gkdltjdus@gmail.com>
2023-01-23 17:58:57 +0900

    로그인 입력 폼 생성

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,9 +1,15 @@
 <template>
   <h2>Login Component </h2>
 
-  <v-btn @click="handleClick1">Generate Key</v-btn>
+  <input v-model="memberId" placeholder="ID" />
 
-  <v-btn>Login</v-btn>
+  <input v-model="memberPswd" placeholder="password" />
+
+  <div>
+    <v-btn @click="handleClick1">Generate Key</v-btn>
+
+    <v-btn @click="login">Login</v-btn>
+  </div>
 
 </template>
 
@@ -11,6 +17,20 @@
 
 import axios from "axios";
 import useMemberInfo from "@/store/useMemberInfo";
+import {ref} from "vue";
+
+const memberId = ref("")
+const memberPswd = ref("")
+
+/** 서버로 데이터 전송 **/
+const login = async () => {
+
+  const res = await axios.post(`https://localhost:8080/api/login`)
+
+  console.log(memberId.value)
+  console.log(memberPswd.value)
+
+}
 
 const handleClick1 = async () => {
 
@@ -19,6 +39,7 @@ const handleClick1 = async () => {
   //{access:xxxx, refresh:xxxx}
   const data = res.data
 
+  console.log("------------token info------------")
   console.log(data)
 
   const {saveInfo} = useMemberInfo()
